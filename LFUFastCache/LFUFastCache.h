5369f62e3a1e249fb3fa928ecd3823a0a058daef
蓝新 <scutryzheng@gmail.com>
2019-06-23 10:43:53 +0800

    Initial Commit

---
+++ LFUFastCache/LFUFastCache.h
@@ -0,0 +1,19 @@
+//
+//  LFUFastCache.h
+//  LFUFastCache
+//
+//  Created by 蓝容 on 2019/6/23.
+//  Copyright © 2019 RongLan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LFUFastCache.
+FOUNDATION_EXPORT double LFUFastCacheVersionNumber;
+
+//! Project version string for LFUFastCache.
+FOUNDATION_EXPORT const unsigned char LFUFastCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LFUFastCache/PublicHeader.h>
+
+
---
+++ LFUFastCacheTests/LFUFastCacheTests.m
@@ -0,0 +1,37 @@
+//
+//  LFUFastCacheTests.m
+//  LFUFastCacheTests
+//
+//  Created by 蓝容 on 2019/6/23.
+//  Copyright © 2019 RongLan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LFUFastCacheTests : XCTestCase
+
+@end
+
+@implementation LFUFastCacheTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
